d1ccf5481e85d3ca5e8434c1ba689c50d58bbcf3
Joan Boulangger <joan.boulangger@gmail.com>
2020-08-17 12:58:08 -0500

    codigo de ejemplo

--- index.py
+++ index.py
@@ -6,16 +6,41 @@ env_mpath = Path ('.')
 load_dotenv (dotenv_path='.env')
 token = os.getenv("TOKEN")
 
-class MyClient (discord.Client):
-    async def on_ready (self):
-        print ('Logged on as {0}!'.format(self.user))
-    
-    async def on_message (self, message):
-        if message.author == self.user:
-            return 
-        
-        if message.content.startswith ('!hello'):
-            await message.channel.send ('Hello')
+client = discord.Client ()
 
-client = MyClient ()
+@client.event
+async def on_ready ():
+    print ('Logged on as {0}!'.format(client))
+    
+@client.event 
+async def on_message (message):
+    if message.author == client.user:
+        return 
+        
+    if message.content.startswith ('!like'):
+        channel = message.channel
+        await channel.send ('Send me that 👍 reaction, mate')
+
+        def check (reaction, user): 
+            return user == message.author and str (reaction.emoji) == '👍'
+            
+        try: 
+            rection, user = await client.wait_for ('reaction_add', timeout = 30, check=check)
+            
+        except asyncio.TimeoutError:
+            await channel.send ('👎')
+            
+        else: 
+            await channel.send ('👍')
+
+    if message.content.startswith ('!saludo'):
+        channel = message.channel
+        await channel.send ('Say hello!')
+
+        def check (m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for ('message', check = check)
+        await channel.send ('Hello @{.author}!'.format (msg))
+
 client.run (token)
